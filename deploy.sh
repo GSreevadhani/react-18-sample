c81523cf762cb3250d066b7aa1db6704541c9907
G.SREEVADHANI <127740281+GSreevadhani@users.noreply.github.com>
2025-02-06 10:11:02 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo hi123
+chmod +x build.sh
+./build.sh
+
+# Login to DockerHub and check for success
+echo "$DOCKER_PASSWORD" | docker login -u "sreevadhani" --password-stdin
+if [ $? -ne 0 ]; then
+    echo "❌ Docker login failed!"
+    exit 1
+fi
+
+# Tag and push the image
+# Tagging the built image with the correct name before pushing
+docker tag app sreevadhani/apps
+
+# Pushing the tagged image to Docker Hub
+docker push sreevadhani/apps
+
+if [ $? -ne 0 ]; then
+    echo "❌ Docker push failed!"
+    exit 1
+fi
